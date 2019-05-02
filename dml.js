35ec0956f8a1b0904d9a70b03afc0dc1a2e0acde
Austin Franks <afranks@elink.bz>
2019-05-02 11:21:29 -0400

    Fixed reference to org variable in dml.js

--- dml.js
+++ dml.js
@@ -24,7 +24,7 @@ module.exports = function(RED) {
         .authenticate(orgResult.connection, orgResult.config)
         .then((oauth) => {
           let dmlResult;
-          const org = orgResult.org;
+          const org = orgResult.connection;
           switch (theAction) {
             case 'insert':
               dmlResult = org.insert(payload);
